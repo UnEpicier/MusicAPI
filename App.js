bc106006424b02ab77ee313dec0afb7dbb71db94
Un Épicier <=>
2023-02-20 10:19:11 +0100

    Member router

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ const all = require("./router/all");
 const id = require("./router/id");
 const name = require("./router/name");
 const creation = require("./router/creation");
+const member = require("./router/member");
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,7 @@ app.use(all);
 app.use(id);
 app.use(name);
 app.use(creation);
+app.use(member);
 
 app.listen(port, () => {
 	console.log(`\x1b[1m\x1b[32mAPI started on port ${port}\x1b[0m`);
---
+++ router/member.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const router = express.Router();
+const mariadb = require("mariadb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = mariadb.createPool({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASS,
+	database: process.env.MYSQL_DB,
+});
+
+router.get("/api/artists/member/:name/:type/:token", async (req, res) => {
+	let result = [];
+
+	if (req.params.name.match(/^[a-zA-Z0-9_ ]*$/) == null) {
+		return res.status(400).json({ error: "Invalid Query: name" });
+	}
+
+	await pool.getConnection().then((conn) => {
+		conn.query("SELECT COUNT(*) AS count FROM user WHERE token = ?", [
+			req.params.token,
+		]).then((rows) => {
+			if (rows[0].count == 0) {
+				return res.status(401).json({ error: "Invalid token" });
+			}
+
+			let formatted = "";
+			if (req.params.type === "start") {
+				formatted = `${req.params.name}%`;
+			} else if (req.params.type === "end") {
+				formatted = `%${req.params.name}`;
+			} else if (req.params.type === "contain") {
+				formatted = `%${req.params.name}%`;
+			} else {
+				return res.status(400).json({ error: "Invalid Query: type" });
+			}
+
+			conn.query(
+				"SELECT DISTINCT group_id FROM `member` WHERE member_name LIKE ?",
+				[`%${req.params.name}%`]
+			)
+				.then((rows) => {
+					for (let i = 0; i < rows.length; i++) {
+						result.push({
+							id: rows[i].group_id,
+						});
+
+						conn.query("SELECT * FROM `group` WHERE id = ?", [
+							rows[i].group_id,
+						]).then((rows) => {
+							result[i].image = rows[0].image;
+							result[i].name = rows[0].name;
+							result[i].creationDate = parseInt(
+								rows[0].creation_date
+							);
+							result[i].firstAlbum = rows[0].first_album;
+						});
+					}
+				})
+				.then(() => {
+					for (let i = 0; i < result.length; i++) {
+						conn.query(
+							"SELECT * FROM `member` WHERE group_id = ?",
+							[result[i].id]
+						).then((rows) => {
+							result[i].members = [];
+							for (let j = 0; j < rows.length; j++) {
+								result[i].members.push(rows[j].member_name);
+							}
+
+							if (i === result.length - 1) {
+								return res.json(result);
+							}
+						});
+					}
+				});
+		});
+	});
+});
+
+/*
+result.push({
+	id: rows[i].id,
+	image: rows[i].image,
+	name: rows[i].name,
+	creationDate: parseInt(rows[i].creation_date),
+	firstAlbum: rows[i].first_album,
+});
+*/
+
+module.exports = router;
