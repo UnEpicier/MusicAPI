766500b6aada20d6bd662a8ca0168040e7fe2e8f
Un Épicier <=>
2023-02-20 10:04:08 +0100

    Creation date router

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ const express = require("express");
 const all = require("./router/all");
 const id = require("./router/id");
 const name = require("./router/name");
+const creation = require("./router/creation");
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,7 @@ app.use(express.static(`${process.cwd()}/assets/`));
 app.use(all);
 app.use(id);
 app.use(name);
+app.use(creation);
 
 app.listen(port, () => {
 	console.log(`\x1b[1m\x1b[32mAPI started on port ${port}\x1b[0m`);
---
+++ router/creation.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const mariadb = require("mariadb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = mariadb.createPool({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASS,
+	database: process.env.MYSQL_DB,
+});
+
+router.get("/api/artists/creation/:creation/:token", async (req, res) => {
+	let result = [];
+
+	if (req.params.creation.match(/^[0-9]+$/) == null) {
+		return res.status(400).json({ error: "Invalid ID" });
+	}
+
+	await pool.getConnection().then((conn) => {
+		conn.query("SELECT COUNT(*) AS count FROM user WHERE token = ?", [
+			req.params.token,
+		]).then((rows) => {
+			if (rows[0].count == 0) {
+				return res.status(401).json({ error: "Invalid token" });
+			}
+
+			conn.query("SELECT * FROM `group` WHERE creation_date LIKE ?", [
+				`%${req.params.creation}%`,
+			])
+				.then((rows) => {
+					for (let i = 0; i < rows.length; i++) {
+						result.push({
+							id: rows[i].id,
+							image: rows[i].image,
+							name: rows[i].name,
+							creationDate: parseInt(rows[i].creation_date),
+							firstAlbum: rows[i].first_album,
+						});
+					}
+				})
+				.then(() => {
+					for (let i = 0; i < result.length; i++) {
+						conn.query(
+							"SELECT * FROM `member` WHERE group_id = ?",
+							[result[i].id]
+						).then((rows) => {
+							result[i].members = [];
+							for (let j = 0; j < rows.length; j++) {
+								result[i].members.push(rows[j].member_name);
+							}
+
+							if (i === result.length - 1) {
+								return res.json(result);
+							}
+						});
+					}
+				});
+		});
+	});
+});
+
+module.exports = router;
